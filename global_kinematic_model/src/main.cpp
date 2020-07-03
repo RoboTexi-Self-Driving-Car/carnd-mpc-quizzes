c3253af6ac82436ce8c52cf485029d4caef87d6e
Peng Xu <robotpengxu@gmail.com>
2020-07-02 17:31:19 -0700

    quiz 1 done

--- global_kinematic_model/src/main.cpp
+++ global_kinematic_model/src/main.cpp
@@ -15,7 +15,7 @@ double rad2deg(double x) { return x * 180 / pi(); }
 const double Lf = 2;
 
 // Return the next state.
-VectorXd globalKinematic(const VectorXd &state, 
+VectorXd globalKinematic(const VectorXd &state,
                          const VectorXd &actuators, double dt);
 
 int main() {
@@ -33,17 +33,35 @@ int main() {
   std::cout << next_state << std::endl;
 }
 
-VectorXd globalKinematic(const VectorXd &state, 
+VectorXd globalKinematic(const VectorXd &state,
                          const VectorXd &actuators, double dt) {
   // Create a new vector for the next state.
   VectorXd next_state(state.size());
 
   /**
-   * TODO: Implement the global kinematic model,
+   * Done: Implement the global kinematic model,
    *   to return the next state from the inputs.
    */
 
   // NOTE: state is [x, y, psi, v] and actuators is [delta, a]
+
+  // Restore the initial state variables
+  double x0 = state[0];
+  double y0 = state[1];
+  double psi0 = state[2];
+  double v0 = state[3];
+
+  // Restore the control inputs
+  double delta = actuators[0];
+  double a = actuators[1];
+
+  // Compute the next state with the global kinematic model
+  double x1 = x0 + v0 * cos(psi0) * dt;
+  double y1 = y0 + v0 * sin(psi0) * dt;
+  double psi1 = psi0 + v0 / Lf * delta * dt;
+  double v1 = v0 + a * dt;
+
+  next_state << x1, y1, psi1, v1;
 
   return next_state;
 }
